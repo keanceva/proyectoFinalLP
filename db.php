be7968ce8868a578aa8932464d281dd7ac5e855c
Joffre Ramirez <joffreramirez.r@hotmail.com>
2021-01-30 18:01:41 -0500

    Creacion de base datos automatica

--- db.php
+++ db.php
@@ -1,5 +1,45 @@
 <?php
     session_start();
+    $servidor= "localhost";
+    $nombreusuario= "root";
+    $password = "";
+
+    $base= "proyectofinal";
+
+    $conexion= new mysqli($servidor, $nombreusuario, $password);
+
+    if($conexion->connect_error){
+        die("Conexión fallida: " . $conexion->connect_error);
+       }   
+    $sql= "CREATE DATABASE proyectofinal";
+    if($conexion->query($sql)===true){
+        echo "Base de datos creado correctamente...";
+    } 
+    $conexion= new mysqli($servidor, $nombreusuario, $password, $base);
+    
+    $tabla= "CREATE TABLE formulario(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre varchar(100) NOT NULL,
+        telefono varchar(10) NOT NULL,
+        mascota varchar(100) NOT NULL,
+        sexo varchar(20) NOT NULL,
+        descripcion text NOT NULL,
+        fecha date NULL,
+        imagen text NULL)";
+
+    if($conexion->query($tabla)===true){
+        echo "tabla creada correctamente...";
+    } 
+
+    $tabla2= "CREATE TABLE users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email varchar(100) NOT NULL,
+        password varchar(100) NOT NULL)";
+        
+    if($conexion->query($tabla2)===true){
+        echo "tabla2 creada correctamente...";
+    } 
+
 
     $conn = mysqli_connect(
         'localhost',
