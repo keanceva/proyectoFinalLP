3f237eb9d96f5188aa28aa849957f5e44734925d
keanceva <keanceva@espol.edu.ec>
2021-01-29 21:18:10 -0500

    cambios menores

--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 <?php include("include/header.php") ?>
 <div class="container p-4">
     <div class="row">
-        <div class="col-md-4">
-            <img src="https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-marron_0.jpg" class="img-fluid">
-        </div>
-        <div class="col-md-8">
-                
+        <div class="col-md-12">
+            <img src="./img/home1-01.png" class="img-fluid">
         </div>
     </div>
 </div>
+
 <?php include("include/footer.php") ?>
